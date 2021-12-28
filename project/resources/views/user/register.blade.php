a09a7fcffd1dedcfd9efe0ffa318ca1fb01352b6
afzalahmmed <ahmmedafzal4@gmail.com>
2021-12-28 09:29:12 +0600

    ...

--- project/resources/views/user/register.blade.php
+++ project/resources/views/user/register.blade.php
@@ -13,18 +13,19 @@
         <div class="form-container">
             <div>
                 <div class="text-center">
-                    <h2 class="title-head hidden-xs">get <span>started</span></h2>
-                     <p class="info-form">Open account for free and start trading Bitcoins now!</p>
+                    <h2 class="title-head hidden-xs">{{__('get')}} <span>{{__('started')}}</span></h2>
+                     <p class="info-form">{{__('Open account for free and start trading Bitcoins now!')}}</p>
                 </div>
 
                 <form>
                     <div class="form-group">
-                        <input class="form-control" name="name" id="name" placeholder="NAME" type="text" required>
+                        <input class="form-control" name="name" id="name" placeholder="{{__('USER NAME')}}" type="text" required>
                     </div>
 
                     <div class="form-group">
-                        <input class="form-control" name="email" id="email" placeholder="EMAIL" type="email" required>
+                        <input class="form-control" name="email" id="email" placeholder="{{__('EMAIL')}}" type="email" required>
                     </div>
+                    
                     <div class="form-group">
                         <input class="form-control" name="password" id="password" placeholder="PASSWORD" type="password" required>
                     </div>
